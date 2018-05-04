975228548c87c7dd8ca0429b8564ccf6a16168c4
Benjamin Mende <bmende@moosehill.com>
2018-05-03 20:06:33 -0400

    move table and view creation into separate functions. make target and reference views instead of tables

--- project.py
+++ project.py
@@ -20,30 +20,47 @@ def kldiv(tgt,ref):
 
 
 
-conn = psycopg2.connect("dbname=census")
-cur = conn.cursor()
-#Create table Adult
-cur.execute("DROP TABLE IF EXISTS Adult;")
-cur.execute("CREATE TABLE Adult (age INT, workclass VARCHAR(50), fnlwgt INT, education VARCHAR(50), education_num INT, marital_status VARCHAR(50), occupation VARCHAR(50), relationship VARCHAR(50), race VARCHAR(50), sex VARCHAR(50), capital_gain INT, capital_loss INT, hours_per_week INT, native_country VARCHAR(50), salary VARCHAR(50));")
-#Fill in table from csv file
-with open('adult.csv','r') as f:
-   cur.copy_from(f,'adult',sep=',')
-#Create Target table
-cur.execute("DROP TABLE IF EXISTS Target;")
-cur.execute("CREATE TABLE Target AS SELECT * FROM Adult WHERE to_tsvector('english',marital_status) @@ to_tsquery('english','married & !never-married');")
-#Create Reference table (Complement of Target)
-cur.execute("DROP TABLE IF EXISTS Reference;")
-#Sample Query for Target
-cur.execute("CREATE TABLE Reference AS SELECT * FROM Adult WHERE to_tsvector('english',marital_status) @@ to_tsquery('english','!married | never-married');")
-cur.execute("select sex,count(age) from target group by sex;")
-tgt = cur.fetchall()
-#Sample Query for Reference
-cur.execute("select sex,count(age) from reference group by sex;")
-ref = cur.fetchall()
-#K-L Divergence value
-print(kldiv(tgt,ref))
-conn.commit()
-cur.close()
-conn.close()
+def create_adult_table(conn, cur):
+   #Create table Adult
+   cur.execute("DROP TABLE IF EXISTS Adult;")
+   cur.execute("CREATE TABLE Adult (age INT, workclass VARCHAR(50), fnlwgt INT, education VARCHAR(50), education_num INT, marital_status VARCHAR(50), occupation VARCHAR(50), relationship VARCHAR(50), race VARCHAR(50), sex VARCHAR(50), capital_gain INT, capital_loss INT, hours_per_week INT, native_country VARCHAR(50), salary VARCHAR(50));")
+   #Fill in table from csv file
+   with open('adult.csv','r') as f:
+      cur.copy_from(f,'adult',sep=',')
+
+   conn.commit()
+
+
+def create_ref_tgt_views(conn, cur)
+   #Create Target table
+   cur.execute("DROP VIEW IF EXISTS Target;")
+   cur.execute("CREATE VIEW Target AS SELECT * FROM Adult WHERE to_tsvector('english',marital_status) @@ to_tsquery('english','married & !never-married');")
+   #Create Reference table (Complement of Target)
+   cur.execute("DROP VIEW IF EXISTS Reference;")
+   #Sample Query for Target
+   cur.execute("CREATE TABLE Reference AS SELECT * FROM Adult WHERE to_tsvector('english',marital_status) @@ to_tsquery('english','!married | never-married');")
+
+   conn.commit()
+
+def find_kld_sex_age(cur):
+   cur.execute("select sex,count(age) from target group by sex;")
+   tgt = cur.fetchall()
+   #Sample Query for Reference
+   cur.execute("select sex,count(age) from reference group by sex;")
+   ref = cur.fetchall()
+   #K-L Divergence value
+   print(kldiv(tgt,ref))
+
+
+if __name__ == "__main__":
+   conn = psycopg2.connect("dbname=final_project")
+   cur = conn.cursor()
+
+   find_kld_sex_age(cur)
+
+
+   conn.commit()
+   cur.close()
+   conn.close()
 
 
